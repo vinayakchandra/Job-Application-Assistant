58a49307e6c6d0c915b94733a02d23cccc76e436
vinayak <vinayak.chandra.suryavanshi@gmail.com>
2025-07-15 01:18:37 +0530

    get summary from session state

--- components/summarize.py
+++ components/summarize.py
@@ -18,18 +18,26 @@ def render():
                     st.session_state.summarize = summarize_github_repo(github_repo_link)
                 st.success("summarized successfully!")
 
-            if st.session_state.summarize:
-                st.markdown("### Repo Description:")
-                st.markdown(st.session_state.summarize)
+        if st.session_state.summarize:
+            st.markdown("### Repo Description:")
+            st.markdown(st.session_state.summarize)
     with tab2:
         if st.button("Generate Post"):
             if not github_repo_link.strip():
                 st.warning("Please paste Github link first!")
             else:
                 with st.spinner("Generating Post..."):
-                    st.session_state.linkedin_post = generate_linkedin_post(github_repo_link)
+                    # repo summary in session
+                    if st.session_state.summarize:
+                        st.session_state.linkedin_post = generate_linkedin_post(st.session_state.summarize)
+                        print("in session")
+                    else:
+                        st.session_state.summarize = summarize_github_repo(github_repo_link)
+                        st.session_state.linkedin_post = generate_linkedin_post(st.session_state.summarize)
+                        print("not in seesion")
+
                 st.success("Generating Post successfully!")
 
-            if st.session_state.linkedin_post:
-                st.markdown("### 🔗 Linkedin Post:")
-                st.markdown(st.session_state.linkedin_post)
+        if st.session_state.linkedin_post:
+            st.markdown("### 🔗 Linkedin Post:")
+            st.markdown(st.session_state.linkedin_post)
